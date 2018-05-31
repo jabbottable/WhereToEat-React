ede5d5242d03d07bf274e30eb30d0b80e3384e15
Joshua Abbott <josh.abbott@live.com>
2018-05-31 20:54:46 +0100

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import ReactGoogleMaps from './Map';
+
+class App extends Component {
+  render() {
+    return (
+      <ReactGoogleMaps/>
+    );
+  }
+}
+
+export default App;
